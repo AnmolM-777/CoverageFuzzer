b6db952c4fcfbb26ab6b3e8430a88850e4153bb1
Anmol Mishra <anmolmishra777@gmail.com>
2025-11-22 17:39:25 +0530

    perf(forkserver): reduce process cloning latency [step 33]

--- include/mutator.h
+++ include/mutator.h
@@ -8,3 +8,5 @@ class Mutator { public: static std::vector<uint8_t> mutate_bytes(const std::vect
 // Incremental development step #7
 
 // Incremental development step #20
+
+// Incremental development step #33
