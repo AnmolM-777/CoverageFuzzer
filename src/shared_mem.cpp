34394793246e3a59e8cc8dd14416c9495ae8b4f2
Anmol Mishra <anmolmishra777@gmail.com>
2025-12-16 21:44:03 +0530

    perf(ipc): align shared memory bitmap buffer to 64 bytes [step 69]

--- src/shared_mem.cpp
+++ src/shared_mem.cpp
@@ -15,3 +15,5 @@ void SharedMemory::clear() { if (trace_bits) std::memset(trace_bits, 0, MAP_SIZE
 // Incremental development step #43
 
 // Incremental development step #56
+
+// Incremental development step #69
