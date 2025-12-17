336d25c91ba0e01467f3c67005ed0213da32d825
Anmol Mishra <anmolmishra777@gmail.com>
2025-12-17 19:29:34 +0530

    feat(llvm): optimize basic block edge counter lookup [step 70]

--- include/forkserver.h
+++ include/forkserver.h
@@ -16,3 +16,5 @@ class Forkserver { std::string target_path; uint8_t* trace_ptr; public: Forkserv
 // Incremental development step #44
 
 // Incremental development step #57
+
+// Incremental development step #70
